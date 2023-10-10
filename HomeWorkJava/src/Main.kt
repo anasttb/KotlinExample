61d92afb6ed6b7aa27748792ec4e4b206d885a2e
Алексей Собенин <saa@renue.ru>
2023-10-10 22:34:35 +0500

    init

--- HomeWorkJava/src/Main.kt
+++ HomeWorkJava/src/Main.kt
@@ -1,2 +1,17 @@
-class Main {
+fun main() {
+
+    var num: Int = 123456
+
+    var res = 0
+
+    while (true) {
+        res += num % 10
+        num /= 10
+        if (num == 0) {
+            break
+        }
+    }
+
+    println(res)
+
 }
